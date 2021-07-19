2d5b87146d0216a21ce5ae499143a35505be10c9
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-07-19 14:50:08 +0530

    Easy - Sum of 2 nums without using '+'

---
+++ sum_of_2_integers.py
@@ -0,0 +1,38 @@
+'''
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+
+Example 1:
+Input: a = 1, b = 2
+Output: 3
+
+Example 2:
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+'''
+
+import math
+def getSum(a,b):
+    return int(math.log2(pow(2, 2) * pow(2, 3))) #math returns a float point we need to type cast it to int
+print(getSum(12,3))
+
+
+# This method is faster than the previous one 
+def getSum2(a,b):
+    if b > 0:
+        while(b>0):
+            a += 1
+            b -= 1
+    elif b < 0:
+        while(b < 0):
+            a -= 1
+            b += 1
+    return a
+
+print(getSum2(12, 14))
+            
